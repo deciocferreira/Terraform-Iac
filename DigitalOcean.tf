57930cedf17e6a27ea827ab433822f87575dc986
Decio Ferreira <decio_ferreira1@hotmail.com>
2022-10-23 20:19:52 -0300

    TFResource

---
+++ DigitalOcean.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "2.23.0"
+    }
+  }
+}
+
+provider "digitalocean" {
+  token = ""
+}
+
+resource "digitalocean_kubernetes_cluster" "foo" {
+  name   = "foo"
+  region = "nyc1"
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.22.8-do.1"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 3
+
+    taint {
+      key    = "workloadKind"
+      value  = "database"
+      effect = "NoSchedule"
+    }
+  }
+}
